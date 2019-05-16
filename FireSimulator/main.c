1ebc7f2f3c967d0d0e9b87903ef417648f3cf4c5
PlugNCS <michael.nass@free.fr>
2019-05-16 18:27:56 +0200

    Initial Commit

---
+++ FireSimulator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FireSimulator
+//
+//  Created by PlugN on 16/05/2019.
+//  Copyright © 2019 Groupe MINASTE. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
